ec9ba5ed5043eacfe6beb263f9a9df5d56a86aac
Roman Bereziuk <roma.physicist@gmail.com>
2024-11-25 10:04:32 +0200

    Remove links to bottom tabs

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,10 @@
 import { StyleSheet, View } from 'react-native';
 import { theme } from '../theme';
 import { ShoppingListItem } from '../components/ShoppingListItem';
-import { Link } from 'expo-router';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Link
-        href="/counter"
-        style={{
-          textAlign: 'center',
-          marginBottom: 18,
-          fontSize: 24,
-          color: '#00BFFF',
-        }}
-      >
-        Go to /counter
-      </Link>
       <ShoppingListItem name="Coffee" />
       <ShoppingListItem name="Apple" />
       <ShoppingListItem name="PlayStation 5" isCompleted />
